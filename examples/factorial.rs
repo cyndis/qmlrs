55ccd0a353ed25a894d2e1b344a3ad7a0d3e41cc
Mikko Perttunen <cyndis@kapsi.fi>
2015-01-04 15:13:07 +0200

    Cleanup example

--- examples/factorial.rs
+++ examples/factorial.rs
@@ -10,17 +10,12 @@ impl Factorial {
     }
 }
 
-Q_OBJECT!( Factorial:
+Q_OBJECT! { Factorial:
     slot fn calculate(int);
-);
+}
 
 fn main() {
     let mut engine = qmlrs::Engine::new();
-
-    let mut path = std::os::getcwd().unwrap();
-    path.push_many(&["examples", "factorial_ui.qml"]);
-    path = std::os::make_absolute(&path).unwrap();
-
     engine.load_local_file(&Path::new("examples/factorial_ui.qml"));
 
     engine.set_property("factorial", Factorial);
