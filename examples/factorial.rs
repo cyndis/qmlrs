857514c4d9508a28f90fddbd4f16f156f932b0e1
Tyler Pickett <t.pickett66@gmail.com>
2016-02-01 08:54:23 -0700

    Split factorial example into two files.
    The first example lets Qt handle reading a file and using it, the second
    example reads the file in and feeds the engine a string.

--- examples/factorial.rs
+++ examples/factorial.rs
@@ -1,9 +1,6 @@
 #[macro_use]
 extern crate qmlrs;
 
-// use std::fs::File;
-// use std::io::prelude::*;
-
 struct Factorial;
 impl Factorial {
     fn calculate(&self, x: i64) -> i64 {
@@ -17,21 +14,10 @@ Q_OBJECT! { Factorial:
 }
 
 fn main() {
-    let mut engine = qmlrs::Engine::new();
+    let engine = qmlrs::Engine::new();
 
     engine.set_property("factorial", Factorial);
     engine.load_local_file("examples/factorial_ui.qml");
 
     engine.exec();
-
-    // test with string:
-    // let mut engine2 = qmlrs::Engine::new();
-    // engine2.set_property("factorial", Factorial);
-    // let mut qml_file = File::open("examples/factorial_ui.qml").unwrap();
-    // let mut qml_string = String::new();
-    // qml_file.read_to_string(&mut qml_string).unwrap();
-    // qml_string = qml_string.replace("Factorial", "Factorial (from string)");
-    // engine2.load_data(&qml_string);
-
-    // engine2.exec();
 }
---
+++ examples/factorial_string.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate qmlrs;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+struct Factorial;
+impl Factorial {
+    fn calculate(&self, x: i64) -> i64 {
+        (1..x+1).fold(1, |t,c| t * c)
+    }
+}
+
+Q_OBJECT! { Factorial:
+    slot fn calculate(i64);
+//    signal fn test();
+}
+
+fn main() {
+    let mut engine = qmlrs::Engine::new();
+    engine.set_property("factorial", Factorial);
+    let mut qml_file = File::open("examples/factorial_ui.qml").unwrap();
+    let mut qml_string = String::new();
+    qml_file.read_to_string(&mut qml_string).unwrap();
+    qml_string = qml_string.replace("Factorial", "Factorial (from string)");
+    engine.load_data(&qml_string);
+
+    engine.exec();
+}
